ee9c53e12c71be49e64b83d9aef249a87cff83c5
Margarita262 <margarita.rod262@gmail.com>
2017-01-18 22:46:02 -0700

    Terminacion de CSS y JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// TODO agregar codigo javascript
+//alert('Hola mundo');
+
+console.log('Hola desde la console de javascript');
+
+function login(){
+    var usuario= document.getElementById('txtusuario').value;
+    var passwd= document.getElementById('txtpassword').value;
+    alert('Usuario:' +usuario+ ', contraseña'+passwd);
+}
